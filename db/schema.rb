fd19633bae1b65f2e94477e32f46cd3f2301c1c3
matt werth <matt werth>
2016-09-07 12:27:49 +1000

    basic layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160907014739) do
+
+  create_table "entries", force: :cascade do |t|
+    t.string   "email"
+    t.string   "full_name"
+    t.string   "address_line_1"
+    t.string   "address_line_2"
+    t.string   "postcode"
+    t.string   "phone"
+    t.string   "store_name"
+    t.string   "terminal_number"
+    t.string   "receipt_number"
+    t.date     "sale_date"
+    t.time     "sale_time"
+    t.string   "birth_year"
+    t.boolean  "subscribe"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
