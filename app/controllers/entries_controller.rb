962ca87cbe9898e7880ec033c43de8853bcef76f
matt werth <matt werth>
2016-09-14 22:04:44 +1000

    bla

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,7 @@
 class EntriesController < ApplicationController
 
   before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  
   before_action :authenticate_user!, only: [:index, :edit, :destroy]
 
 
