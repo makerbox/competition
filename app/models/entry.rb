5f901ea0daef69a94b9ec9f897c87afb230bcdca
matt werth <matt werth>
2016-09-16 14:26:30 +1000

    subscribe to csv

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,7 +2,7 @@ class Entry < ApplicationRecord
 	  validates :email, :full_name, :address_line_1, :postcode, :phone, :store_name, :terminal_number, :receipt_number, :sale_date, :sale_time, :birth_year, presence: true
  
  def self.to_csv
-    attributes = %w{id email full_name address_line_1 postcode phone store_name terminal_number receipt_number sale_date sale_time birth_year}
+    attributes = %w{id email full_name address_line_1 postcode phone store_name terminal_number receipt_number sale_date sale_time birth_year subscribe}
 
     CSV.generate(headers: true) do |csv|
       csv << attributes
