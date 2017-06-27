91ed0820f145c11be4fcbc19ad7856dfdc34f721
matt werth <matt werth>
2017-06-27 12:53:23 +1000

    push to linux

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
-	  validates :email, :full_name, :address_line_1, :postcode, :phone, :store_name, :terminal_number, :receipt_number, :sale_date, :sale_time, :birth_year, presence: true
+	  validates :email, :full_name, :address_line_1, :postcode, :phone, :store_name, :receipt_number, :sale_date, presence: true
  
  def self.to_csv
     attributes = %w{id email full_name address_line_1 postcode phone store_name terminal_number receipt_number sale_date sale_time birth_year subscribe}
