6f290c9fc3134269bf3e0aa183b5b26e038b2284
matt werth <matt werth>
2017-06-28 16:59:28 +1000

    heroku

--- app/mailers/congrats_mailer.rb
+++ app/mailers/congrats_mailer.rb
@@ -1,2 +1,8 @@
 class CongratsMailer < ApplicationMailer
+	  default from: 'competition@yokohamae.com'
+  		layout 'congrats_mailer'
+
+  	def congrats(entry)
+		mail(to: entry.email, subject: 'your entry has been received')
+	end
 end
