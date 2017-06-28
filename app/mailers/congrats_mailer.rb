2c0151e47f89746d3aea20a7debee1ac2ac8b047
matt werth <matt werth>
2017-06-28 17:39:49 +1000

    terms link in email

--- app/mailers/congrats_mailer.rb
+++ app/mailers/congrats_mailer.rb
@@ -2,6 +2,7 @@ class CongratsMailer < ApplicationMailer
 	  default from: 'competition@yokohama.com'
 
   	def congrats_mailer(entry)
+  		@termslink = terms_index_path
 		mail(to: entry.email, subject: 'your entry has been received')
 	end
 end
